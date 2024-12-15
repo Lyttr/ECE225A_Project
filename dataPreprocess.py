2d2bde1d83fba2b8d6b564e1f7e2ae14a831a8c8
Lyttr <jgy2001@126.com>
2024-12-15 03:12:03 -0800

    update

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -2,6 +2,18 @@ import pandas as pd
 import dataset
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import nltk
 RawRecipe=dataset.getRawRecipes()
+def numDataAnalyRecipe():
+    print(RawRecipe.describe()['minutes'],RawRecipe.describe()['n_steps'],RawRecipe.describe()['n_ingredients'])
+def wordFrequencyRecipe(visable:bool):
+    textName = ' '.join(RawRecipe['name'])
+    if(visable):
+        wordcloud = WordCloud(width=800, height=400, background_color='white').generate(textName)
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.title('Word frequency in NAME field')
+        plt.axis('off')
+        plt.show()
 
-print(RawRecipe.describe()['minutes'],RawRecipe.describe()['n_steps'],RawRecipe.describe()['n_ingredients'])
+wordFrequencyRecipe(1)
